bc05d3c9159efcd2f381c50c00a5d0056c4ee647
Mingxv Hu <1983729691@qq.com>
2024-08-02 10:50:24 +0800

    1

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <STC89C5xRC.H>
-#define SW1 P42	//按键SW1 定义
-#define SW2 P43	//按键SW2 定义
+#define SW1 P42
 
-//需求：独立按键控制LED灯的移动
-//延时函数：作用->消除抖动
+//独立按键控制LED显示二进制数，每次按下SW1，二进制位+1
+//延时函数：消除抖动
 void Delay10ms()		//@11.0592MHz
 {
 	unsigned char i, j;
@@ -18,36 +17,22 @@ void Delay10ms()		//@11.0592MHz
 
 void main()
 {
-	unsigned char i = 0;   // 局部变量要付给初始值，否则是随机数i
-	P0 = ~0x01;
-	while (1)
+	unsigned char i = 0;
+	while(1)
 	{
-		if (SW1 == 0)      		// 检查按键SW1
+		//0000 0001
+		//0000 0011
+		//0000 0111
+		if(SW1 == 0)
 		{
-			Delay10ms();   			// 延时消抖
-			while (SW1 == 0);  // 等待按键释放
-			Delay10ms();       // 延时消抖
-				
+			Delay10ms();
+			while(SW1 == 0);
+			Delay10ms();
 			i++;
-			if(i>=8)
-				i=0;
-			
-			P0 = ~(0x01<<i);		
-			}
-		
-		if(SW2 == 0)
-		{
-			Delay10ms();
-			while(SW2 == 0);
-			Delay10ms();
-			
-			if(i==0)
-				i = 7;
-			else
-				i--;
-			P0 = ~(0x01<<i); //这里左移是左移i位，如右移1位，相当于左移7位
-			//例如i=3.此时第四个LED亮，右移1位，此时i = 2，第三个灯亮
+			P0 = ~i; 
+			//1、按下去 i = 1，此时 0000 0001
+			//2、i+1 = 2，此时 0000 0010
+			//...
 		}
-		}
+	}
 }
-
